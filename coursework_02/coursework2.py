5ba8947ff21fec52122dcbd14f23a7cb7dddce4f
w09524br <bogdan-gabriel.rotaru@student.manchester.ac.uk>
2020-11-24 17:44:50 +0200

    Initialising the window for the game

---
+++ coursework_02/coursework2.py
@@ -0,0 +1,14 @@
+#Coursework 2 Game 
+
+from tkinter import Tk 
+
+def configure_window():
+	window.geometry("900x700")
+	window.configure(background="black")
+	window.title("MY  GAME")
+
+window = Tk()
+
+configure_window()
+
+window.mainloop()
